abd7018db5695b69a416681c8cff2a04bbc62c3f
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2022-12-28 23:19:11 +0530

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 exports.connectDatabase = () => {
-    mongoose.connect("mongodb://localhost:27017/machineAdmin").
+    mongoose.connect("mongodb+srv://pritam:tTcc2jdrRSveXo2J@cluster0.ivwjaoi.mongodb.net/MachineDatabase").
     then((con) => console.log("database connected ",con.connection.host))
     .catch((err) => console.error(err))
 }
