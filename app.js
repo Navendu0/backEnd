aa495c115a5045eb2fd0a4be3c91d37bb6b5fdd1
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2022-12-29 23:59:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173", // <-- location of the react app were connecting to
+    origin: "https://ms-maity.onrender.com", // <-- location of the react app were connecting to
     credentials: true,
   })
 );
