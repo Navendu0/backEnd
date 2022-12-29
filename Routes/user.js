9da170c5c82aea5e7a887e11324f16720caaa785
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2022-12-29 19:46:40 +0530

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const { registerUser, logout, login, profile } = require("../Controller/user")
+const { isAuth } = require("../isauth")
 
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 
-router.route('/login').post(login)
+router.route('/login').post(login,isAuth)
 
 router.route('/logout').get(logout)
 
