4a9e614b55d6e45ad60e7ea7d65d889074354fb1
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2023-01-04 20:16:10 +0530

    Update featurePhotos.js

--- Controller/featurePhotos.js
+++ Controller/featurePhotos.js
@@ -1,25 +1,50 @@
 const cloudinary = require("cloudinary")
 
+
 const FeaturePhotos = require("../Models/FeaturePhotos")
 
 exports.addFeaturePhotos = async (req, res) => {
     try {
         if (req.files) {
             let data = [];
+            let featurePhotos = await FeaturePhotos.findOne({})
+
             for (var i = 0; i < req.files.length; i++) {
+
                 const result = await cloudinary.uploader.upload(req.files[i].path);
                 data.push({
                     public_id: result.public_id,
                     url: result.secure_url,
                 });
             }
-            const featurePhotos = await FeaturePhotos.create(data)
-            res.status(201).json({ sucess: true, featurePhotos, message: "feature photos added sucessfully" });
+
+            if (featurePhotos) {
+                console.log(data)
+                data.forEach(items => {
+                    featurePhotos.photos.push(items)
+
+                })
+                await featurePhotos.save()
+            }
+
+            if (!featurePhotos) {
+                featurePhotos = await FeaturePhotos.create({ photos: data })
+            }
+
+            return res.status(201).json({ sucess: true, featurePhotos, message: "feature photos added sucessfully" });
+        }
+
+        if (!req.files) {
+            return res.status(403).json({
+                success: false,
+                message: "no image found"
+            })
         }
 
 
 
     } catch (error) {
+        console.error(error)
         res.status(500).json({
             sucess: false,
             message: error.message,
@@ -33,17 +58,17 @@ exports.addFeaturePhotos = async (req, res) => {
 exports.deleteFeaturePhotos = async (req, res) => {
     try {
         const { id } = req.body
-        const featurePhotos = await FeaturePhotos.findById({_id:id})
-        if(featurePhotos.length == 0){
+        const featurePhotos = await FeaturePhotos.findById({ _id: id })
+        if (featurePhotos.length == 0) {
             return res.status(403).json({
-                success:false,
-                message:"no feature photos found "
+                success: false,
+                message: "no feature photos found "
             })
-        }  
+        }
 
         featurePhotos.photos.forEach((item) => {
             cloudinary.uploader.destroy(item.public_id);
-          });
+        });
 
         return res.status(200).json({
             success: true,
@@ -61,14 +86,3 @@ exports.deleteFeaturePhotos = async (req, res) => {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
