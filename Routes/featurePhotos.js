a397880262d2364ef6ea240b3cc20f79e44ff460
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2023-01-04 19:00:31 +0530

    feature photos route add

---
+++ Routes/featurePhotos.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const { addFeaturePhotos, deleteFeaturePhotos } = require("../Controller/featurePhotos")
+const { isAuth } = require("../isauth")
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    // destination: (req, file, cb) => {
+    //     cb(null, 'uploads')
+    // },
+    filename: (req, file, cb) => {
+      
+        cb(null, Date.now()+'-'+file.originalname )
+    }
+});
+  
+var upload = multer({ storage: storage });
+
+
+
+const router = express.Router()
+
+router.route('/uploadfeaturephotos').post(isAuth,upload.array("photos",10),addFeaturePhotos)
+router.route('/deletefeaturephotos').post(isAuth,deleteFeaturePhotos)
+
+
+
+
+module.exports = router
